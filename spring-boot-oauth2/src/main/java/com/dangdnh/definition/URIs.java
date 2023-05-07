837fdd3b46edc092c39f675252820a9224ac6999
dnhdang94 <dnhdang94@gmail.com>
2023-05-07 15:26:30 +0700

    feat (spring-boot-oauth2): add google oauth2

---
+++ spring-boot-oauth2/src/main/java/com/dangdnh/definition/URIs.java
@@ -0,0 +1,10 @@
+package com.dangdnh.definition;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class URIs {
+    public static final String ROOT = "/demo";
+    public static final String HOME = ROOT + "/home";
+}
